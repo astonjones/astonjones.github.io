c3a2458cc9cba9354414b42e3cc6196267eadf39
Aston <astonbradleyjones@gmail.com>
2020-03-15 14:46:49 -0500

    This is the saving of the files prior to uploading the repository to github.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
 import PersonIcon from '@material-ui/icons/Person';
 import WorkIcon from '@material-ui/icons/Work';
 import ChatIcon from '@material-ui/icons/Chat';
@@ -7,17 +6,33 @@ import ChatIcon from '@material-ui/icons/Chat';
 class Navigation extends Component{
     render() {
         return(
-            <Navbar className="container" bg="primary" variant="dark" expand="lg">
-                <Navbar.Brand href="/">Aston</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
-                        <Nav.Link href="/about"><PersonIcon /> About me</Nav.Link>
-                        <Nav.Link href="/work"><WorkIcon /> Work</Nav.Link>
-                        {/* <Nav.Link href="/contact"><ChatIcon /></Nav.Link> */}
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+            // <Navbar className="container" bg="primary" variant="dark" expand="lg">
+            //     <Navbar.Brand href="/">Aston</Navbar.Brand>
+            //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            //     <Navbar.Collapse id="basic-navbar-nav">
+            //         <Nav className="ml-auto">
+            //             <Nav.Link href="/about"><PersonIcon /> About me</Nav.Link>
+            //             <Nav.Link href="/work"><WorkIcon /> Work</Nav.Link>
+            //             {/* <Nav.Link href="/contact"><ChatIcon /></Nav.Link> */}
+            //         </Nav>
+            //     </Navbar.Collapse>
+            // </Navbar>
+            <nav className="myNav">
+                <ul className="nav-list">
+                    <li className="nav-item">
+                        <a href="#" className="nav-link"><span className="icon"><PersonIcon /></span></a>
+                        <span className="link-text">About Me</span>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#" className="nav-link"><span className="icon"><WorkIcon /></span></a>
+                        <span className="link-text">Work</span>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#" className="nav-link"><span className="icon"><ChatIcon /></span></a>
+                        <span className="link-text">Contact Me</span>
+                    </li>
+                </ul>
+            </nav>
         )
     }
 }
